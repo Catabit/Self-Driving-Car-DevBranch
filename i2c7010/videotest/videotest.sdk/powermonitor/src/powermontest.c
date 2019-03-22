6599c156edec8298e9399f891a6503c556452e1b
ionascuandrei <andrei.ionascu.97@gmail.com>
2019-03-22 15:48:41 +0200

    PowerMonitor skel in progress

---
+++ i2c7010/videotest/videotest.sdk/powermonitor/src/powermontest.c
@@ -0,0 +1,30 @@
+#include "powermon.h"
+
+
+void Run(int fd) {
+	//int16_t x, y, z;
+	float current = 0, voltage = 0;
+
+	printf("Pmon Demo Launched");
+
+	while(1){
+
+	}
+
+}
+
+void Initialize(int fd) {
+	SetContConversion(fd);
+	printf("Initialize worked\n");
+}
+
+
+int main()
+{
+	int fd = init();
+	printf("Let's start\n");
+	Initialize(fd);
+	Run(fd);
+
+	return 0;
+}
